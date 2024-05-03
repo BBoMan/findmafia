762f7edcf5f7e7d362f558b2d066cfae89dfd854
BBoMan <parksuga2001@gmail.com>
2024-05-03 22:01:48 +0900

    finish

--- Please/launch.c
+++ Please/launch.c
@@ -7,22 +7,22 @@
 int main(int argc, char *argv[]) {
     int pid;
     if((pid = fork()) == 0) {   //child part
-        printf("launch : start sumfact %s\n", argv[1]);
-        execlp("./sumfact", "sumfact", argv[1], NULL); //start sumfact, 
+        printf("launch : start sumfact %s\n", argv[1]); //sumfact print
+        execlp("./sumfact", "sumfact", argv[1], NULL); //start sumfact with parameter 
         printf("execlp failed!\n");    //if execlp() failed, print the message
         return -1;
     }
     else if(pid > 0) {  //parent part
-        int sum = 0;    //total sum 1~N
-        for(int i=0; i < atoi(argv[1]); i++) {
+        int sum = 0;    //variable of sumation
+        for(int i=0; i < atoi(argv[1]); i++) { //sumation of 1~N
             sum += (i+1);
         }
-        printf("laundch : sum = %d\n", sum);    //print the sum
-        waitpid(pid, NULL, 0);
+        printf("laundch : sum = %d\n", sum);    //print the total sum
+        waitpid(pid, NULL, 0);                  //wait until child finish
         return 0;
     }
     else {
-        printf("fork() is failed\n");
+        printf("fork() is failed\n");           //fork failed
         return -1;
     }
 }
--- Please/sumfact.c
+++ Please/sumfact.c
@@ -4,38 +4,38 @@
 #include<unistd.h>
 #include<pthread.h>
 
-void *threadFact(void *N) {
+void *threadFact(void *N) { //factorial function
     int num = atoi(N);
-    int factorial = 1;
-    for(int i=1; i<=num; i++) {
+    int factorial = 1;  //variable of factorial
+    for(int i=1; i<=num; i++) { //factorial of 1~N
         factorial *= i;
     }
-    printf("sumfact : factorial = %d\n", factorial);
+    printf("sumfact : factorial = %d\n", factorial); // print factorial result
 
     return NULL;
 }
 
-void *threadSum(void *N) {
+void *threadSum(void *N) {  //sumation function
     int num = atoi(N);
-    int sum = 0;
-    for(int i=1; i<=num; i++) {
+    int sum = 0;            //variable of sumation
+    for(int i=1; i<=num; i++) { //sumation of 1~N
         sum += i;
     }
-    printf("sumfact : sum = %d\n", sum);
+    printf("sumfact : sum = %d\n", sum); //print sumation result
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    pthread_t tid[atoi(argv[1])];
+    pthread_t tid[atoi(argv[1])];   //array size is parameter num
 
-    pthread_create(&tid[0], NULL, threadFact, (void *)argv[1]);
-    pthread_join(tid[0], NULL);
+    pthread_create(&tid[0], NULL, threadFact, (void *)argv[1]); //first thread call factorial function
+    pthread_join(tid[0], NULL); //sumfact process can't finish until this thread finish
 
     for(int i=1; i<atoi(argv[1]); i++) {
-        pthread_create(&tid[i], NULL, threadSum, (void *)argv[1]);
+        pthread_create(&tid[i], NULL, threadSum, (void *)argv[1]); //extra thread call sumation function
     }
 
-    for(int i=1; i<atoi(argv[1]); i++) {
+    for(int i=1; i<atoi(argv[1]); i++) { //until all thread finish, sumfact process can't finish
         pthread_join(tid[i], NULL);
     }
     return 0;
